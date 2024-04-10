35c15b853f18442fc8c2a9b0b8f7dade37fe617e
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-04-10 14:31:58 +0200

    Add try catch

--- app.js
+++ app.js
@@ -4,36 +4,48 @@ const fs = require('fs');
 
 
 const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
-    const image = await Jimp.read(inputFile);
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-    const txtData = {
-        text,
-        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-    };
+    try {
+        const image = await Jimp.read(inputFile);
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+        const txtData = {
+            text,
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        };
 
-    image.print(font, 0, 0, txtData, image.getWidth(), image.getHeight());
-    await image.quality(100).writeAsync(outputFile);
-    
-    console.log("Watermark added. Path: " + outputFile);
-    startApp();
+        image.print(font, 0, 0, txtData, image.getWidth(), image.getHeight());
+        await image.quality(100).writeAsync(outputFile);
+
+        console.log("Watermark added. Path: " + outputFile);
+        startApp();
+    }
+    catch (error){
+        console.log('Something went wrong... Try again!');
+        startApp();
+    }
 };
 
 const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile){
-    const image = await Jimp.read(inputFile);
-    const watermark = await Jimp.read(watermarkFile);
-    watermark.resize(50, Jimp.AUTO)
-    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
-    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+    try {
+        const image = await Jimp.read(inputFile);
+        const watermark = await Jimp.read(watermarkFile);
+        watermark.resize(50, Jimp.AUTO)
+        const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+        const y = image.getHeight() / 2 - watermark.getHeight() / 2;
 
-    image.composite(watermark, x, y, {
-        mode: Jimp.BLEND_SOURCE_OVER,
-        opacitySource: 0.5,
-    });
+        image.composite(watermark, x, y, {
+            mode: Jimp.BLEND_SOURCE_OVER,
+            opacitySource: 0.5,
+        });
 
-    await image.quality(100).writeAsync(outputFile);
-    console.log("Watermark added. Path: " + outputFile);
-    startApp();
+        await image.quality(100).writeAsync(outputFile);
+        console.log("Watermark added. Path: " + outputFile);
+        startApp();
+    }
+    catch (error) {
+        console.log('Something went wrong... Try again!');
+        startApp();
+    }
 };
 
 const prepereOutputFilename = (filname) => {
@@ -78,6 +90,7 @@ const startApp = async () => {
             addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepereOutputFilename(options.inputImage), options.watermarkText);
         } else {
             console.log('Ups something went wrong... Try again');
+            startApp();
         }
       }
       else {
@@ -92,6 +105,7 @@ const startApp = async () => {
         addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepereOutputFilename(options.inputImage), './img/' + options.watermarkImage)
         :
         console.log('Ups something went wrong... Try again');
+        startApp();
       }
 
 };
